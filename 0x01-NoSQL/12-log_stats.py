bacbc2e4de7482dce1c203cac29dd6dff68aec8d
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2023-02-15 04:33:21 +0100

    finishing up

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,16 +6,18 @@ module for the 12th task
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-collection = client.logs.nginx
-
-print("{} logs".format(collection.count_documents({})))
-print("Methods:")
-print("\tmethod GET: {}".format(collection.count_documents({"method": "GET"})))
-print("\tmethod POST: {}".format(
-    collection.count_documents({"method": "POST"})))
-print("\tmethod PUT: {}".format(collection.count_documents({"method": "PUT"})))
-print("\tmethod PATCH: {}".format(
-    collection.count_documents({"method": "PATCH"})))
-print("\tmethod DELETE: {}".format(
-    collection.count_documents({"method": "DELETE"})))
+if __name__ == '__main__':
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+    print("\tmethod GET: {}".format(
+        collection.count_documents({"method": "GET"})))
+    print("\tmethod POST: {}".format(
+        collection.count_documents({"method": "POST"})))
+    print("\tmethod PUT: {}".format(collection.count_documents(
+        {"method": "PUT"})))
+    print("\tmethod PATCH: {}".format(
+        collection.count_documents({"method": "PATCH"})))
+    print("\tmethod DELETE: {}".format(
+        collection.count_documents({"method": "DELETE"})))
